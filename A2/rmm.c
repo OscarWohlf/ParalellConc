07d1904894dc14e9d26c79c9945b32ab8e2e2c95
PabloSarro <pablo.sarrosanchez@epfl.ch>
2026-04-28 23:31:33 +0200

    ProgE Implementation

--- A2/rmm.c
+++ A2/rmm.c
@@ -20,7 +20,9 @@ int main(int argc, char *argv[]) {
     // Initialise MPI
     MPI_Init(&argc, &argv);
     
-    /* Step 1: Read the values of M, N and K from the command line arguments. */
+    // --------------------------------------------------------
+    // ----------- STEP 1: READ COMMAND LINE ARGS -------------
+    // --------------------------------------------------------
     int M = atoi(argv[1]);
     int N = atoi(argv[2]);
     int K = atoi(argv[3]);
@@ -36,66 +38,228 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    /* Step 2: Generates and initializes matrices A and B with random values. */
+    // --------------------------------------------------------
+    // ------ STEP 2: GENERATE A, B AND DISTRIBUTE DATA -------
+    // --------------------------------------------------------
+
     int *matA[M];
     int *matB[N];
     int *matC[M/2];
 
-    // Only rank 0
-    init_mat(matA, M, N, 0);
+    // Only rank 0 initialises matrices A and C (since other processes only need specific rows of A, which will be scattered later).
+    if (rank==0) {
+        init_mat(matA, M, N, 0);
+        init_mat(matC, M/2, K/2, -1);
+    }
+    // All ranks initialise matrix B (since each of them require the whole matrix).
     init_mat(matB, N, K, 1);
-    init_mat(matC, M/2, K/2, -1);
 
-    // Scatter A and broadcast B to other processes
-    
+    // Flatten A, for easier scattering (since matA is non-contiguous in memory).
+    int *A_flat = NULL;
+    if(rank == 0) { // A is flattened only on rank 0, since other processes will receive the info via MPI_Scatterv.
+        A_flat = (int *)malloc(M * N * sizeof(int));
+        // Copy non-contiguous matA into contiguous A_flat
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < N; j++) {
+                A_flat[i*N + j] = matA[i][j];
+            }
+        }
+    }
+    // Also flatten B for better cache performance (cache locality) when accessing the values during RMM computation. Done for all ranks, since every processor will need access to it.
+    int *B_flat = (int *)malloc(N * K * sizeof(int));
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < K; j++) {
+            B_flat[i*K + j] = matB[i][j];
+        }
+    }
+
+    // --------------------------------------------------------
+    // ----------------- 2.1 SCATTER A'S ROWS -----------------
+    // --------------------------------------------------------
+
+    // Input arg 1: Send buffer
+    int* sendbuf_scat = A_flat;
+    // Input args 2 & 3: Send counts (n_elems to be received x proc) and displacements (starting idx of elems to be sent to each proc)
+    int* sendcounts = NULL;
+    int* displs = NULL;
+    int total_rows = M / 2;
+    int rows_per_proc = total_rows / nprocs;
+    int remaining_unasign_rows = total_rows % nprocs;
+    int p_rows;
+
+    if(rank == 0) {
+        sendcounts = (int *)malloc(nprocs * sizeof(int));
+        displs = (int *)malloc(nprocs * sizeof(int));
+        for(int p = 0; p < nprocs; p++) {
+            if (p < remaining_unasign_rows) { // we will assign an extra row to the first 'remaining_unasign_rows' processes, to ensure all of A's rows are covered
+                p_rows = rows_per_proc + 1; // Number of rows given to each process (with the extra one)
+                sendcounts[p] = 2 * p_rows * N; // We need to send pairs of rows (RMM) and each row of A has N elements.
+            } else {
+                p_rows = rows_per_proc; // Number of rows now is just the previous number (no extra)
+                sendcounts[p] = 2 * p_rows * N;
+            }
+            if (p == 0) {
+                displs[p] = 0; // First process starts at the beginning of the array
+            } else {
+                displs[p] = displs[p-1] + sendcounts[p-1]; // Starting index for process p is the previous displacement + previous count
+            }
+        }
+    }
+    // Input arg 4: Send type
+    MPI_Datatype sendtype_scat = MPI_INT;
+    // Input arg 5: Receive buffer (need to recalculate the rows per each rank/processor)
+    int local_rows;
+    if (rank < remaining_unasign_rows) {
+        local_rows = rows_per_proc + 1;
+    } else {
+        local_rows = rows_per_proc;
+    }
+    int *A_recv = (int *)malloc(2 * local_rows * N * sizeof(int));
+    // Input arg 6: Receive count (number of elements to be received)
+    int recvcount = 2 * local_rows * N; // Explained previously.
+    // Input arg 7: Receive type
+    MPI_Datatype recvtype_scat = MPI_INT;
+    // Input arg 8: Root process (the one that scatters the data)
+    int root_scat = 0;
+    // Input arg 9: Communicator
+    MPI_Comm comm_scat = MPI_COMM_WORLD;
+
+    MPI_Scatterv(
+        sendbuf_scat, sendcounts, displs, 
+        sendtype_scat, A_recv, recvcount, 
+        recvtype_scat, root_scat, comm_scat
+    );
+
+    // Allocate space for a local matrix C per each process, which will store the partial results.
+    int *local_C = (int *)calloc(local_rows * (K/2), sizeof(int)); // Flat 1D array.
+
     if(debug) {
         display_matrix(matA, M, N, "A");
         display_matrix(matB, N, K, "B");
     }
 
-    /* Step 3: Computes the matrix C as the RMM of matrices A and B. */
-    /* Parallelize and optimize this part only! */
-    printf("Starting Computation...\n");
-    set_clock();
+    // --------------------------------------------------------
+    // ----------------- STEP 3: COMPUTE RMM ------------------
+    // --------------------------------------------------------
 
-    int total_rows = M / 2;
-    int rows_per_proc = total_rows / nprocs;
-    int remainder = total_rows % nprocs;
-    int start_row = rank * rows_per_proc + (rank < remainder ? rank : remainder);
-    int local_rows = rows_per_proc + (rank < remainder ? 1 : 0);
-    
+    /* Parallelise and optimise this part only! */
+    if (rank==0) {
+        printf("Starting Computation...\n");
+    }
+    // Ensure all processes are ready to start the computation before starting the clock.
     MPI_Barrier(MPI_COMM_WORLD);
-    
-    for(int idx = start_row; idx < start_row + local_rows; idx++) {
-        int *c_row = matC[idx];
-        int *a0_row = matA[idx * 2];
-        int *a1_row = matA[idx * 2 + 1];
+    set_clock();
         
-        for(int kdx = 0; kdx < N; kdx++) {
-            int a0 = a0_row[kdx];
-            int a1 = a1_row[kdx];
-            int *b_row = matB[kdx];
-            
-            for(int jdx = 0; jdx < K/2; jdx++) {
-                int b0 = b_row[jdx * 2];
-                int b1 = b_row[jdx * 2 + 1];
+    for(int m = 0; m < local_rows; m++) {
+        int *c_row = &local_C[m * (K/2)];
+        int *a0_row = &A_recv[2*m * N];
+        int *a1_row = &A_recv[(2*m+1) * N];
+        
+        for(int n = 0; n < N; n++) {
+            int a0 = a0_row[n];
+            int a1 = a1_row[n];
+            int *b_row = &B_flat[n * K]; // n-th row of B.
+            int a_sum = a0 + a1; // Precompute a0+a1 sum outside of j loop.
 
-                c_row[jdx] += (a0+a1) * (b0+b1);
+            for(int k = 0; k < K/2; k++) {
+                int b0 = b_row[k * 2];
+                int b1 = b_row[k * 2 + 1];
+
+                c_row[k] += a_sum * (b0+b1);
             }
         }
     }
 
-    // Gather C from all processes
+    // --------------------------------------------------------
+    // ------------- 3.1 GATHER C'S LOCAL RESULTS -------------
+    // --------------------------------------------------------
+
+    // Input arg 1: Send buffer
+    int* sendbuf_gath = &local_C[0];
+    // Input arg 2: Send count (n_elems to be sent per proc)
+    int sendcount = local_rows * (K/2);
+    // Input arg 3: Send type
+    MPI_Datatype sendtype_gath = MPI_INT;
+    // Input arg 4: Receive buffer (flattened C)
+    int *recvbuf = NULL;
+    if(rank == 0) {
+        recvbuf = (int *)malloc((M/2) * (K/2) * sizeof(int));
+    }
+    // Input arg 5 & 6: Receive counts (n_elems to be received per proc) and displacements (starting idx of elems to be received from each proc)
+    int* recvcounts = NULL;
+    // displs is reused from previous scatter, hence no need to allocate it again.
+    if(rank == 0) {
+        recvcounts = (int *)malloc(nprocs * sizeof(int));
+        for(int p = 0; p < nprocs; p++) {
+            if (p < remaining_unasign_rows) {
+                p_rows = rows_per_proc + 1;
+                recvcounts[p] = p_rows * (K/2);
+            } else {
+                p_rows = rows_per_proc; // Number of rows now is just the previous number (no extra)
+                recvcounts[p] = p_rows * (K/2);
+            }
+            if (p == 0) {
+                displs[p] = 0; // First process starts at the beginning of the array
+            } else {
+                displs[p] = displs[p-1] + recvcounts[p-1]; // Starting index for process p is the previous displacement + previous count
+            }
+        }
+    }
+    // Input arg 7: Receive type
+    MPI_Datatype recvtype_gath = MPI_INT;
+    // Input arg 8: Root process (the one that gathers the data)
+    int root_gath = 0;
+    // Input arg 9: Communicator
+    MPI_Comm comm_gath = MPI_COMM_WORLD;
+
+    MPI_Gatherv(
+        sendbuf_gath, sendcount, sendtype_gath,
+        recvbuf, recvcounts, displs,
+        recvtype_gath, root_gath, comm_gath
+    );
+
     double totaltime = elapsed_time();
 
-    /* Step 4: Write matrix C into a csv file matC.csv and exit. */
+    // --------------------------------------------------------
+    // ---------------- STEP 4: WRITE C IN CSV ----------------
+    // --------------------------------------------------------
+
     if (rank==0) {
+        for(int i = 0; i < M/2; i++) {
+            for(int j = 0; j < K/2; j++) {
+                matC[i][j] = recvbuf[i*(K/2) + j];
+            }
+        }
         printf("Computation Done!\n");
         if(debug)
             display_matrix(matC, M/2, K/2, "C");
         printf("- Using %d procs: matC computed in %.4gs.\n", nprocs, totaltime);
         write_csv(matC, M/2, K/2, "matC.csv");
     }
+
+    // Cleanup.
+    if(rank == 0) {
+        for(int i = 0; i < M; i++) {
+            free(matA[i]);
+        }
+        for(int i = 0; i < M/2; i++) {
+            free(matC[i]);
+        }
+    }
+    for(int i = 0; i < N; i++) {
+        free(matB[i]);
+    }
+    if (rank==0) {
+        free(A_flat);
+        free(sendcounts);
+        free(displs);
+        free(recvcounts);
+        free(recvbuf);
+    }
+    free(B_flat);
+    free(A_recv);
+    free(local_C);
+
 
     MPI_Finalize();
     return 0;
